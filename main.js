147e32c68cc7ca27d475301a3d0fb6c2e4db8eb3
Clem_undev <39344736+LightAnge@users.noreply.github.com>
2021-08-09 23:53:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ const { exec } = require("child_process");
 const execFile =[
   "searchByName_Libération",
   "searchByName_LeFigaro",
-//   "searchByName_ER",
   "searchByName_LeMonde",
+  //"searchByName_ER",
   //"searchByName_LHumanité"
 ]
-const search = "covid";
+
+
+
+if( typeof process.argv[2] === "undefined"){
+  console.log("please specify a query");
+  const search = "baguette";
+}
+else{
+  search = process.argv[2];
+}
 
 execFile.forEach(element => 
   exec("node "+element+".js "+search+" "+search+element.replace("searchByName","")+".log", (error, stdout, stderr) => {    
